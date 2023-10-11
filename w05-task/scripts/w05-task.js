5be88c9cec2f2b9ba41345fb2410858e00159963
lauralandonlovell <roryboo@gmail.com>
2023-10-11 08:34:16 +0200

    solving issues with github pages

--- w05-task/scripts/w05-task.js
+++ w05-task/scripts/w05-task.js
@@ -1,15 +1,23 @@
 /* W05: Programming Tasks */
-
 /* Declare and initialize global variables */
-
+const templesElement = document.getElementById("#temples");
+const templeList = [];
 
 /* async displayTemples Function */
+const displayTemples = (temples)  
+temples.forEach((temple) => 
+{
+    document.createElement("article")
+    document.createElement()
+    console.log(temple);
+});
 
-
-
-
-/* async getTemples Function using fetch()*/
-
+/* async getTemples Function using fetch()--This site helped me understand the basic syntax better dmitripavlutin.com/javascript-fetch-async-await/*/
+async function fetchTemples()
+{
+    const gettingDisplayTemples = await fetch("/displayTemples");
+    return await waitForTemples.json();
+}
 
 /* reset Function */
 
